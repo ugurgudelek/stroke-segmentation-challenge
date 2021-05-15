bacbad137d6ba5db0d41d7438560a7f5c27be359
Gorkem Can Ates <g.canates@gmail.com>
2021-05-16 02:51:19 +0300

    Update metrics v2

--- StrokeMetrics/stroke_metrics.py
+++ StrokeMetrics/stroke_metrics.py
@@ -3,45 +3,49 @@ from torch import nn
 
 
 class Precision(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-6):
         super().__init__()
+        self.eps = eps
 
     def forward(self, yhat, y):
         preds = torch.round(yhat).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FP = torch.sum((preds == 1) * (y == 0))
-        return TP / (TP + FP)
+        return TP / (TP + FP + self.eps)
 
 
 class Recall(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-6):
         super().__init__()
+        self.eps = eps
 
     def forward(self, yhat, y):
         preds = torch.round(yhat).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FN = torch.sum((preds == 0) * (y == 1))
-        return TP / (TP + FN)
+        return TP / (TP + FN + self.eps)
 
 
 class FPR(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-6):
         super().__init__()
+        self.eps = eps
 
     def forward(self, yhat, y):
         preds = torch.round(yhat).detach()
         FP = torch.sum((preds == 1) * (y == 0))
         TN = torch.sum((preds == 0) * (y == 0))
-        return FP / (FP + TN)
+        return FP / (FP + TN + self.eps)
 
 
 class F1(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-6):
         super().__init__()
+        self.eps = eps
 
     def forward(self, yhat, y):
         preds = torch.round(yhat).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FN = torch.sum((preds == 0) * (y == 1))
         FP = torch.sum((preds == 1) * (y == 0))
-        return TP / (TP + 0.5 * (FP + FN))
+        return TP / (TP + 0.5 * (FP + FN) + self.eps)

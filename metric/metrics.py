f8fdc4f5ab28267f3790202d4a091950adddaa87
Gorkem Can Ates <g.canates@gmail.com>
2021-05-19 17:59:47 +0300

    Updated metric v3

--- metric/metrics.py
+++ metric/metrics.py
@@ -7,8 +7,8 @@ class Accuracy(nn.Module):
         super().__init__()
 
     def forward(self, yhat, y):
-        preds = torch.round(yhat).detach()
-        acc = torch.sum(preds == y)/yhat.size(0)
+        preds = (torch.argmax(yhat, dim=1)).detach()
+        acc = (torch.sum(preds == y)) / yhat.size(0)
         return acc.cpu().numpy().item()
 
 
@@ -18,7 +18,7 @@ class Precision(nn.Module):
         self.eps = eps
 
     def forward(self, yhat, y):
-        preds = torch.round(yhat).detach()
+        preds = (torch.argmax(yhat, dim=1)).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FP = torch.sum((preds == 1) * (y == 0))
         return (TP / (TP + FP + self.eps)).cpu().numpy().item()
@@ -30,22 +30,22 @@ class Recall(nn.Module):
         self.eps = eps
 
     def forward(self, yhat, y):
-        preds = torch.round(yhat).detach()
+        preds = (torch.argmax(yhat, dim=1)).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FN = torch.sum((preds == 0) * (y == 1))
         return (TP / (TP + FN + self.eps)).cpu().numpy().item()
 
 
-class FPR(nn.Module):
+class Specificity(nn.Module):
     def __init__(self, eps=1e-6):
         super().__init__()
         self.eps = eps
 
     def forward(self, yhat, y):
-        preds = torch.round(yhat).detach()
+        preds = (torch.argmax(yhat, dim=1)).detach()
         FP = torch.sum((preds == 1) * (y == 0))
         TN = torch.sum((preds == 0) * (y == 0))
-        return (FP / (FP + TN + self.eps)).cpu().numpy().item()
+        return 1 - (FP / (FP + TN + self.eps)).cpu().numpy().item()
 
 
 class MeanMetric(nn.Module):
@@ -53,12 +53,12 @@ class MeanMetric(nn.Module):
     def __init__(self):
         super().__init__()
         self.recall = Recall()
-        self.fpr = FPR()
+        self.specificity = Specificity()
 
     def forward(self, yhat, y):
         recall = self.recall(yhat, y)
-        fpr = self.fpr(yhat, y)
-        return (recall + fpr) / 2
+        specificity = self.specificity(yhat, y)
+        return (recall + specificity) / 2
 
 
 class F1(nn.Module):
@@ -67,7 +67,7 @@ class F1(nn.Module):
         self.eps = eps
 
     def forward(self, yhat, y):
-        preds = torch.round(yhat).detach()
+        preds = (torch.argmax(yhat, dim=1)).detach()
         TP = torch.sum((preds == 1) * (y == 1))
         FN = torch.sum((preds == 0) * (y == 1))
         FP = torch.sum((preds == 1) * (y == 0))

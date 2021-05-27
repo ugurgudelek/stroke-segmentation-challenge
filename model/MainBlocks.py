42770e79349d1cb179b11ef7d87d4fd13fbfb10f
Görkem Can Ateş <76211399+gorkemcanates@users.noreply.github.com>
2021-05-28 01:09:55 +0300

    Classificationexperiment (#10)
    * XNET- debug test needed
    
    * XNET debug test completed
    
    * ResUnet debug test completed
    
    Co-authored-by: Uğur Güdelek <ugurgudelek@gmail.com>

--- model/MainBlocks.py
+++ model/MainBlocks.py
@@ -47,16 +47,16 @@ class DSconv_block(BaseModel):
         nn.Module.__init__(self)
         self.norm_type = norm_type
         self.DSconv = DepthWiseConv2D(in_features)
-        if self.norm_type  == 'gn':
+        if self.norm_type == 'gn':
             self.norm = nn.GroupNorm(32 if in_features >= 32 else in_features, in_features)
-        if self.norm_type  == 'bn':
+        if self.norm_type == 'bn':
             self.norm = nn.BatchNorm2d(in_features)
         self.relu = nn.ReLU()
         self.conv2d = conv_block(in_features, out_features, kernel_size=1, padding=0, norm_type=norm_type)
 
     def forward(self, x):
         x = self.DSconv(x)
-        if self.norm_type  is not None:
+        if self.norm_type is not None:
             x = self.norm(x)
         x = self.relu(x)
         x = self.conv2d(x)
@@ -89,20 +89,23 @@ class FSM(BaseModel):
         channel_num = x.shape[1]
         res = x
         x = conv_block(
-            in_features=int(channel_num), out_features=int(channel_num // 8), norm_type=self.norm_type).to(self.device)(x)
+            in_features=int(channel_num), out_features=int(channel_num // 8), norm_type=self.norm_type).to(self.device)(
+            x)
 
         ip = x
         batchsize, channels, dim1, dim2 = ip.shape
         intermediate_dim = int(channels // 2)
 
-        theta = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
+        theta = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(
+            self.device)(ip)
         theta = torch.reshape(theta, (batchsize, -1, intermediate_dim))
 
-        phi = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
+        phi = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(
+            ip)
         phi = torch.reshape(phi, (batchsize, -1, intermediate_dim))
 
         f = torch.bmm(theta, phi.view(batchsize, intermediate_dim, phi.shape[1]))
-        f = f / (float(f.shape[-1]))
+        f = f / (float(f.shape[1]))
 
         g = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
         g = torch.reshape(g, (batchsize, -1, intermediate_dim))
@@ -115,4 +118,35 @@ class FSM(BaseModel):
         x = conv_block(in_features=channels, out_features=int(channel_num), norm_type=self.norm_type).to(self.device)(x)
         x += res
 
+        return x
+
+
+class ResConv(BaseModel):
+    def __init__(self, in_features, out_features, kernel_size=3, padding=1, stride=1, norm_type=None):
+        nn.Module.__init__(self)
+        self.norm_type = norm_type
+        if self.norm_type == 'gn':
+            self.norm1 = nn.GroupNorm(32 if (in_features >= 32 and in_features%32==0) else in_features, in_features)
+            self.norm2 = nn.GroupNorm(32 if (out_features >= 32 and out_features%32==0) else out_features, out_features)
+
+        if self.norm_type == 'bn':
+            self.norm1 = nn.BatchNorm2d(in_features)
+            self.norm2 = nn.BatchNorm2d(out_features)
+
+        self.pack = nn.Sequential(self.norm1,
+                                  nn.ReLU(),
+                                  nn.Conv2d(in_channels=in_features, out_channels=out_features,
+                                            kernel_size=kernel_size, stride=stride, padding=padding),
+                                  self.norm2,
+                                  nn.ReLU(),
+                                  nn.Conv2d(in_channels=out_features, out_channels=out_features,
+                                            kernel_size=kernel_size, stride=1, padding=1))
+        self.skip = nn.Sequential(nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=kernel_size,
+                                            stride=stride, padding=1),
+                                  self.norm2)
+
+    def forward(self, x):
+        res = self.skip(x)
+        x = self.pack(x)
+        x += res
         return x

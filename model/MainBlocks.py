d5d1803cac1eca3e1173ce5a8d45c112cd4d8206
Gorkem Can Ates <g.canates@gmail.com>
2021-05-27 13:57:41 +0300

    XNET debug test completed

--- model/MainBlocks.py
+++ model/MainBlocks.py
@@ -1,3 +1,10 @@
+# -*- encoding: utf-8 -*-
+# @File    :   MainBlocks.py
+# @Time    :   2021/05/26 23:12:27
+# @Author  :   Gorkem Can Ates
+# @Contact :   g.canates@gmail.com
+# @Desc    :   None
+
 from berries.model.base import BaseModel
 import torch
 import torch.nn as nn
@@ -7,26 +14,27 @@ class conv_block(BaseModel):
     def __init__(self, in_features, out_features, kernel_size=3, padding=1, norm_type=None):
         nn.Module.__init__(self)
         self.conv = nn.Conv2d(in_features, out_features, kernel_size=kernel_size, stride=1, padding=padding)
-        if norm_type == 'gn':
-            self.norm = nn.GroupNorm(32, out_features)
-        if norm_type == 'bn':
+        self.norm_type = norm_type
+        if self.norm_type == 'gn':
+            self.norm = nn.GroupNorm(32 if out_features >= 32 else out_features, out_features)
+        if self.norm_type == 'bn':
             self.norm = nn.BatchNorm2d(out_features)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.conv(x)
-        if self.norm is not None:
+        if self.norm_type is not None:
             x = self.norm(x)
         x = self.relu(x)
         return x
 
 
 class DepthWiseConv2D(BaseModel):
-    def __init__(self, in_features, out_features, kernels_per_layer=1):
+    def __init__(self, in_features, kernels_per_layer=1):
         nn.Module.__init__(self)
         self.depthwise = nn.Conv2d(
-            in_features, out_features * kernels_per_layer, kernel_size=3, padding=1, groups=in_features, dilation=1)
-        self.pointwise = nn.Conv2d(out_features * kernels_per_layer, out_features, kernel_size=1)
+            in_features, in_features * kernels_per_layer, kernel_size=3, padding=1, groups=in_features, dilation=1)
+        self.pointwise = nn.Conv2d(in_features * kernels_per_layer, in_features, kernel_size=1)
 
     def forward(self, x):
         x = self.depthwise(x)
@@ -37,17 +45,18 @@ class DepthWiseConv2D(BaseModel):
 class DSconv_block(BaseModel):
     def __init__(self, in_features, out_features, norm_type):
         nn.Module.__init__(self)
-        self.DSconv = DepthWiseConv2D(in_features, in_features)
-        if norm_type == 'gn':
-            self.norm = nn.GroupNorm(32, in_features)
-        if norm_type == 'bn':
+        self.norm_type = norm_type
+        self.DSconv = DepthWiseConv2D(in_features)
+        if self.norm_type  == 'gn':
+            self.norm = nn.GroupNorm(32 if in_features >= 32 else in_features, in_features)
+        if self.norm_type  == 'bn':
             self.norm = nn.BatchNorm2d(in_features)
         self.relu = nn.ReLU()
-        self.conv2d = conv_block(in_features, out_features, kernel_size=1, padding=0, norm=norm_type)
+        self.conv2d = conv_block(in_features, out_features, kernel_size=1, padding=0, norm_type=norm_type)
 
     def forward(self, x):
         x = self.DSconv(x)
-        if self.norm is not None:
+        if self.norm_type  is not None:
             x = self.norm(x)
         x = self.relu(x)
         x = self.conv2d(x)
@@ -56,49 +65,54 @@ class DSconv_block(BaseModel):
 
 class x_block(BaseModel):
     def __init__(self, in_features, out_features, norm_type):
-        nn.Module.__init__()
+        nn.Module.__init__(self)
         self.res = conv_block(in_features, out_features, kernel_size=1, padding=0, norm_type=norm_type)
-        self.dc = DSconv_block(out_features, out_features, norm_type=norm_type)
+        self.dc1 = DSconv_block(in_features, out_features, norm_type=norm_type)
+        self.dc2 = DSconv_block(out_features, out_features, norm_type=norm_type)
 
     def forward(self, x):
         res = self.res(x)
-        for i in range(3):
-            x = self.dc(x)
+        x = self.dc1(x)
+        x = self.dc2(x)
+        x = self.dc2(x)
         x += res
         return x
 
 
 class FSM(BaseModel):
-    def __init__(self, norm_type):
-        nn.Module.__init__()
+    def __init__(self, norm_type, device):
+        nn.Module.__init__(self)
         self.norm_type = norm_type
+        self.device = device
 
     def forward(self, x):
-        channel_num = x.shape[-1]
+        channel_num = x.shape[1]
         res = x
-        x = conv_block(channel_num, int(channel_num // 8), self.norm_type)(x)
+        x = conv_block(
+            in_features=int(channel_num), out_features=int(channel_num // 8), norm_type=self.norm_type).to(self.device)(x)
+
         ip = x
         batchsize, channels, dim1, dim2 = ip.shape
-        intermediate_dim = channels // 2
+        intermediate_dim = int(channels // 2)
 
-        theta = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False)(ip)
+        theta = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
         theta = torch.reshape(theta, (batchsize, -1, intermediate_dim))
 
-        phi = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False)(ip)
+        phi = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
         phi = torch.reshape(phi, (batchsize, -1, intermediate_dim))
 
         f = torch.bmm(theta, phi.view(batchsize, intermediate_dim, phi.shape[1]))
         f = f / (float(f.shape[-1]))
 
-        g = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False)(ip)
+        g = nn.Conv2d(int(channel_num // 8), intermediate_dim, kernel_size=1, padding=0, bias=False).to(self.device)(ip)
         g = torch.reshape(g, (batchsize, -1, intermediate_dim))
 
         y = torch.bmm(f, g)
         y = torch.reshape(y, (batchsize, intermediate_dim, dim1, dim2))
-        y = nn.Conv2d(intermediate_dim, channels, kernel_size=1, padding=0, bias=False)(y)
+        y = nn.Conv2d(intermediate_dim, channels, kernel_size=1, padding=0, bias=False).to(self.device)(y)
         y += ip
         x = y
-        x = conv_block(in_features=channels, out_features=int(channel_num), norm_type=self.norm_type)(x)
+        x = conv_block(in_features=channels, out_features=int(channel_num), norm_type=self.norm_type).to(self.device)(x)
         x += res
 
         return x

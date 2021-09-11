ea96675240e6a3551db054d1e30b4974f98add1e
Uğur Güdelek <ugurgudelek@gmail.com>
2021-09-11 15:00:14 +0300

    Segmentation is ready for contest

---
+++ checkpoints/classification/170/stroke-experiment.py
@@ -0,0 +1,130 @@
+# -*- encoding: utf-8 -*-
+# @File    :   stroke-experiment.py
+# @Time    :   2021/05/15 16:45:06
+# @Author  :   Ugur Gudelek, Gorkem Can Ates
+# @Contact :   ugurgudelek@gmail.com
+# @Desc    :   None
+
+from pathlib import Path
+import torch
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+
+from berries.experiments.experiment import Experiment
+from berries.metric import metrics
+from berries.logger import MultiLogger
+
+from dataset.stroke_classification import StrokeClassificationDataset
+from model.cnn import VGG16, CNN, DenseNet, ResNet, CustomCNN
+from trainer.demo import DemoTrainer
+from metric import metrics as local_metrics
+from dataset.stroke import Stroke
+from loss.losses import WeightedBCELoss
+
+
+class StrokeExperiment(Experiment):
+    def __init__(self):
+        self.params = {
+            'project_name': 'stroke',
+            'experiment_name': 'ResNet-50-WBCE-lr1e-4-bsize-16-pretrained-0-dataaug-2-TL-0',
+            # 'project_name': 'debug',
+            # 'experiment_name': 'process',
+            'seed': 42,
+            'device': 'cuda' if torch.cuda.is_available() else 'cpu',
+
+            'resume': False,
+            'pretrained': False,
+            'checkpoint': {
+                'on_epoch': 2,
+            },
+            'log': {
+                'on_epoch': 1,
+            },
+            'stdout': {
+                'verbose': True,
+                'on_batch': 0,
+                'on_epoch': 1
+            },
+            'root': Path('./'),
+            'neptune': {
+                # 'id': 'DEBUG-12',
+                'workspace': 'machining',
+                'project': 'stroke',
+                'tags': ['Stroke', 'ResNet-50-WBCE-lr1e-4-bsize-16-pretrained-0-dataaug-2-TL-0'],
+                'source_files': ['./stroke-experiment.py']
+            }
+        }
+
+        self.hyperparams = {
+            'lr': 0.0001,
+            'weight_decay': 0.,
+            'epoch': 100,
+            'batch_size': 16,
+            'validation_batch_size': 16,
+        }  # yapf: disable
+
+        self.alpha = 0.333
+        # Create netcdf4 file for faster reading
+        # Stroke(root=Path('./input/stroke'))
+
+        # self.model = CustomCNN(in_channels=3, out_channels=2, input_dim=(3, 512, 512))
+        # self.model = VGG16(pre_trained=False, req_grad=True, bn=False, out_channels=1, input_dim=(3, 512, 512))
+        self.model = ResNet(net_type='ResNet-50', pre_trained=False, req_grad=True, out_channels=2,
+                            input_dim=(3, 512, 512))
+        # self.model = DenseNet(net_type='densenet-121', pre_trained=False, req_grad=True, out_channels=2,
+        #                     input_dim=(3, 512, 512))
+        print(self.model)
+
+        self.dataset = StrokeClassificationDataset(Path('../stroke-segmentation-challenge/input/stroke'),
+                                                   transform=transforms.Compose([transforms.RandomApply(
+                                                       [transforms.RandomRotation((-180, 180)),
+                                                        transforms.ColorJitter(),
+                                                        transforms.RandomHorizontalFlip(p=0.5),
+                                                        transforms.RandomVerticalFlip(p=0.5),
+                                                        transforms.GaussianBlur(kernel_size=51)], p=0.5)]),
+                                                   test_size=0.25)
+
+        self.logger = MultiLogger(
+            root=self.params['root'],
+            project_name=self.params['project_name'],
+            experiment_name=self.params['experiment_name'],
+            params=self.params,
+            hyperparams=self.hyperparams)
+
+        self.trainer = DemoTrainer(
+            model=self.model,
+            criterion=torch.nn.CrossEntropyLoss(weight=torch.tensor([self.alpha, 1 - self.alpha]).to('cuda')),
+            # criterion=WeightedBCELoss([self.alpha, 1-self.alpha]),
+            metrics=[local_metrics.Accuracy,
+                     local_metrics.MeanMetric,
+                     local_metrics.Recall,
+                     local_metrics.Specificity],
+            # metrics=[metrics.Accuracy],
+
+            hyperparams=self.hyperparams,
+            params=self.params,
+            logger=self.logger
+        )
+
+    def run(self):
+        self.trainer.fit(dataset=self.dataset.trainset,
+                         validation_dataset=self.dataset.testset)
+
+        # Log final model
+        self.logger.log_model(path=self.trainer._get_last_checkpoint_path())
+
+        # Log prediction dataframe
+        prediction_dataframe = self.trainer.to_prediction_dataframe(dataset=self.dataset.testset,
+                                                                    classification=True,
+                                                                    save=True)  # yapf:disable
+
+        self.logger.log_dataframe(key='prediction/validation',
+                                  dataframe=prediction_dataframe)
+
+        # Log image
+        # Example can be found at trainer.py
+
+
+if __name__ == '__main__':
+    experiment = StrokeExperiment()
+    experiment.run()
---
+++ classification-test.py
@@ -0,0 +1,102 @@
+from mimetypes import init
+import warnings
+
+warnings.filterwarnings("ignore", module="matplotlib\..*")
+
+from pathlib import Path
+from cv2 import log
+import torch
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+
+import pandas as pd
+
+from berries.experiments.experiment import Experiment
+from berries.metric import metrics
+from berries.logger import MultiLogger
+from berries.trainer.base import BaseTrainer
+
+from dataset.stroke import StrokeClassificationDataset, StrokeClassificationTorch
+from model.cnn import VGG16, CNN, DenseNet, ResNet, CustomCNN
+
+from metric import classification as local_metrics
+from dataset.stroke import Stroke
+
+PNG_PATH = Path('input/teknofest/contest/input/classification/PNG')
+# PNG_PATH = Path('input/teknofest/raw/KANAMA/PNG')
+PRETRAINED_MODEL_PATH = Path(
+    './checkpoints/classification/170/model-optim.pth')
+OUTPUT_PATH = Path('input/teknofest/contest/output/classification')
+HOLD_PROBABILITY = 1.
+
+params = {
+    'resume': False,
+    'pretrained': True,
+    'pretrained_path': PRETRAINED_MODEL_PATH,
+    'device': 'cuda'
+}
+
+alpha = 0.333
+# Create netcdf4 file for faster reading
+# Stroke(root=Path('./input/stroke'))
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = ResNet(net_type='ResNet-50',
+               pre_trained=False,
+               req_grad=True,
+               out_channels=2,
+               input_dim=(3, 512, 512))
+print(model)
+
+# transform = transforms.Compose([
+#     transforms.RandomApply([
+#         transforms.RandomRotation((-180, 180)),
+#         transforms.ColorJitter(),
+#         transforms.RandomHorizontalFlip(p=0.5),
+#         transforms.RandomVerticalFlip(p=0.5),
+#         transforms.GaussianBlur(kernel_size=51)
+#     ],
+#                            p=0.5)
+# ])
+
+xr_dataset = Stroke.from_path_classification(png_path=PNG_PATH,
+                                             infer_label=True,
+                                             hold_prop=HOLD_PROBABILITY)
+torch_dataset = StrokeClassificationTorch(xr_dataset.image.values,
+                                          xr_dataset.label.values,
+                                          transform=None)
+
+trainer = BaseTrainer(model=model,
+                      criterion=torch.nn.CrossEntropyLoss(
+                          weight=torch.tensor([alpha, 1 - alpha]).to(device)),
+                      metrics=[
+                          local_metrics.Accuracy(),
+                          local_metrics.MeanMetric(),
+                          local_metrics.Recall(),
+                          local_metrics.Specificity()
+                      ],
+                      hyperparams=dict(),
+                      params=params,
+                      optimizer=None,
+                      scheduler=None,
+                      logger=None)
+
+OUTPUT_PATH.mkdir(exist_ok=True, parents=True)
+
+predictions, targets = trainer.transform(dataset=torch_dataset,
+                                         classification=True)
+prediction_dataframe = pd.DataFrame({
+    'ID': xr_dataset.id.values,
+    'ETiKET': predictions.squeeze(),
+    # 'target': targets.squeeze()
+})# yapf:disable
+
+prediction_dataframe.to_csv(OUTPUT_PATH / 'OTURUM1.csv', index=False)
+
+print(prediction_dataframe)
+
+score, _ = trainer.score(dataset=torch_dataset,
+                         batch_size=16,
+                         classification=False)
+print(score)
